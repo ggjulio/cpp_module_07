18772a7c6fee0f0a16e804d51a6775dffa4a41af
ggjulio <juligonz@student.42.fr>
2021-02-14 23:56:47 +0100

    ex01 seem done

--- ex01/includes/iter.hpp
+++ ex01/includes/iter.hpp
@@ -6,14 +6,17 @@
 /*   By: juligonz <juligonz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/14 20:53:32 by juligonz          #+#    #+#             */
-/*   Updated: 2021/02/14 20:53:52 by juligonz         ###   ########.fr       */
+/*   Updated: 2021/02/14 23:18:38 by juligonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ITER_H
 #define ITER_H
 
-
-
+template<typename T>
+void	iter(T array[], size_t size, void (*func)(T &)){
+	for (size_t i = 0; i < size; i++)
+		func(array[i]);	
+}
 
 #endif
--- ex01/srcs/main.cpp
+++ ex01/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: juligonz <juligonz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/14 19:07:25 by juligonz          #+#    #+#             */
-/*   Updated: 2021/02/14 20:53:07 by juligonz         ###   ########.fr       */
+/*   Updated: 2021/02/14 23:56:14 by juligonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,64 @@
 #include <string>
 #include "iter.hpp"
 
+template<typename T, size_t SIZE>
+void print(const T (&to_print)[SIZE]){
+	std::cout << "{";
+	for (size_t i = 0; i < SIZE; i++)
+	{
+		if (i > 0)
+			std::cout << ", ";
+		std::cout << to_print[i];
+	}
+	std::cout << "}" << std::endl;
+}
+
+void funcInt(int &i){
+	i += i-3;
+}
+
+void funcstring(std::string &s){
+	s += "_NEW";
+}
+
+template<typename T>
+void funcTemplate(const T &s){
+	s += s;
+}
+
 int main()
 {
+	std::cout << std::endl << "###########################################" << std::endl
+		<< "############# Test int ###################" <<  std::endl;
+
+	{
+		int arr[] = {1,2,3,4,5};
+
+		print(arr);
+		iter(arr, 5, funcInt);
+		print(arr);
+	}
+	std::cout << std::endl << "###########################################" << std::endl
+		<< "############# Test String ###################" <<  std::endl;
+	{
+		std::string arr[] = {"A","B","C","D","E"};
+
+		print(arr);
+		iter(arr, 5, ::funcstring);
+		print<std::string>(arr);
+	}
+	
+	std::cout << std::endl << "###########################################" << std::endl
+		<< "############# Test String ###################" <<  std::endl;
+	{
+		std::string arr[] = {"A","B","C","D","E"};
+
+		print(arr);
+		
+		iter(arr, 5, &funcTemplate<std::string &>);
+		print(arr);
+		iter(arr, 5, &funcTemplate<std::string &>);
+		print(arr);
+	}
 	return 0;
 }
