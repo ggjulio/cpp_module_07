6dd9ac5defb9b697a123f901aa2e66520d8415f0
ggjulio <juligonz@student.42.fr>
2021-02-14 21:05:31 +0100

    ex00 refacto

--- ex01/includes/iter.hpp
+++ ex01/includes/iter.hpp
@@ -1,34 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   whatever.hpp                                       :+:      :+:    :+:   */
+/*   iter.hpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: juligonz <juligonz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/02/14 19:59:50 by juligonz          #+#    #+#             */
-/*   Updated: 2021/02/14 21:05:02 by juligonz         ###   ########.fr       */
+/*   Created: 2021/02/14 20:53:32 by juligonz          #+#    #+#             */
+/*   Updated: 2021/02/14 20:53:52 by juligonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef WHATEVER_H
-#define WHATEVER_H
+#ifndef ITER_H
+#define ITER_H
 
-template<typename T>
-void swap(T & left, T & right)
-{
-	T tmp = left;
-	left = right;
-	right = tmp;
-}
 
-template<typename T>
-T &	min(T & left, T & right){
-	return left < right ? left : right;	
-}
 
-template<typename T>
-T &	max(T & left, T & right){
-	return left > right ? left : right;
-}
 
 #endif
--- ex01/srcs/main.cpp
+++ ex01/srcs/main.cpp
@@ -1,34 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   whatever.hpp                                       :+:      :+:    :+:   */
+/*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: juligonz <juligonz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/02/14 19:59:50 by juligonz          #+#    #+#             */
-/*   Updated: 2021/02/14 21:05:02 by juligonz         ###   ########.fr       */
+/*   Created: 2021/02/14 19:07:25 by juligonz          #+#    #+#             */
+/*   Updated: 2021/02/14 20:53:07 by juligonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef WHATEVER_H
-#define WHATEVER_H
+#include <iostream>
+#include <string>
+#include "iter.hpp"
 
-template<typename T>
-void swap(T & left, T & right)
+int main()
 {
-	T tmp = left;
-	left = right;
-	right = tmp;
+	return 0;
 }
-
-template<typename T>
-T &	min(T & left, T & right){
-	return left < right ? left : right;	
-}
-
-template<typename T>
-T &	max(T & left, T & right){
-	return left > right ? left : right;
-}
-
-#endif
